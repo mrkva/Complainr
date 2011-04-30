29fd7a9e71535516e432df6d982fd075e1208966
Tomas Zeleninsky <mrqwaxgo@gmail.com>
2011-04-30 02:49:11 +0200

    major updates

--- vote.php
+++ vote.php
@@ -1,6 +1,8 @@
 <?php
-
+  include("config.php");
+  $ip=$_SERVER['REMOTE_ADDR']; 
   $id = $_GET['ID'];
+  $voting = $_GET['VOTING'];
   $id = new MongoId($id);
   // open connection to MongoDB server
   $conn = new Mongo('localhost');
@@ -12,14 +14,31 @@
   $collection = $db->items;
 
   $cursor = $collection->find(array( '_id' => $id));
-  foreach ($cursor as $obj) { 
-  	$newData = array('$inc' => array('votes' => 1));
-	$collection->update(array( '_id' => $id), $newData);
-  	}
-  foreach ($cursor as $obj) {
-  	echo "<font color='red'><i>" . $obj['votes'] . " vote(s)</i></font>";
-  	}
   
+  if ($voting == "TRUE") {
+	foreach ($cursor as $obj) { 
+		if ($obj['ip'] == $ip) {
+		 break;
+		} else {
+			$newData = array('$inc' => array('votes' => 1));
+			$collection->update(array( '_id' => $id), $newData);
+			$ipData = array('$set' => array('ip' => $ip));
+			$collection->update(array( '_id' => $id), $ipData);
+		}
+  	}
+  	
+	foreach ($cursor as $obj) {
+		echo "<font color='red'><i>" . $obj['votes'] . " vote(s)</i></font>";
+  	}
+  } else {
+  	foreach ($cursor as $obj) { 
+  		if ($obj['ip'] == $ip) {
+  	 		echo "<font color='red'><i>" . $obj['votes'] . " vote(s)</i></font>";
+  		} else {
+  	 		echo "<a href='#' onClick='vote($(this).parent()[0].id);'>" . $obj['votes'] . " Vote(s)</a>";
+  		}
+  	}
+  }
   // disconnect from server
   $conn->close();
 
